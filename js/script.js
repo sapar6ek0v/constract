e8a48549d007931c3d9f16ea8ae21efa6fc8049a
sapar6ek0v <razetka0309@gmail.com>
2021-10-08 15:31:52 +0300

    change footer title-2 and add buttone

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const menu = document.getElementById('ul-header-menu')
 button.addEventListener('click', () =>{
     menu.classList.toggle('header-menu-ul')
 })
+
+const links = document.querySelectorAll('.header-link')
+
+links.forEach( (elem) =>{
+        elem.addEventListener('click', () =>{
+            menu.classList.remove('header-menu-ul')
+        })
+    }
+)
